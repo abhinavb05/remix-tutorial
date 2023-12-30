6ce4ece970a770f25c8e00b69f5936a019dc2e91
abhinav.bansal <abhinavb0512@gmail.com>
2023-12-30 10:22:29 +0530

    get and set contacts from DB

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,75 +1,75 @@
-import { Form } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+
 
 import type { ContactRecord } from "../data";
+import { getContact } from "../data";
+
+export const loader = async ({ params }) => {
+    const contact = await getContact(params.contactId);
+    return json({ contact });
+};
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
-
-  return (
-    <div id="contact">
-      <div>
-        <img
-          alt={`${contact.first} ${contact.last} avatar`}
-          key={contact.avatar}
-          src={contact.avatar}
-        />
-      </div>
-
-      <div>
-        <h1>
-          {contact.first || contact.last ? (
-            <>
-              {contact.first} {contact.last}
-            </>
-          ) : (
-            <i>No Name</i>
-          )}{" "}
-          <Favorite contact={contact} />
-        </h1>
-
-        {contact.twitter ? (
-          <p>
-            <a
-              href={`https://twitter.com/${contact.twitter}`}
-            >
-              {contact.twitter}
-            </a>
-          </p>
-        ) : null}
-
-        {contact.notes ? <p>{contact.notes}</p> : null}
+    const { contact } = useLoaderData<typeof loader>();
+    return (
+        <div id="contact">
+        <div>
+            <img
+            alt={`${contact.first} ${contact.last} avatar`}
+            key={contact.avatar}
+            src={contact.avatar}
+            />
+        </div>
 
         <div>
-          <Form action="edit">
-            <button type="submit">Edit</button>
-          </Form>
+            <h1>
+            {contact.first || contact.last ? (
+                <>
+                {contact.first} {contact.last}
+                </>
+            ) : (
+                <i>No Name</i>
+            )}{" "}
+            <Favorite contact={contact} />
+            </h1>
 
-          <Form
-            action="destroy"
-            method="post"
-            onSubmit={(event) => {
-              const response = confirm(
-                "Please confirm you want to delete this record."
-              );
-              if (!response) {
-                event.preventDefault();
-              }
-            }}
-          >
-            <button type="submit">Delete</button>
-          </Form>
+            {contact.twitter ? (
+            <p>
+                <a
+                href={`https://twitter.com/${contact.twitter}`}
+                >
+                {contact.twitter}
+                </a>
+            </p>
+            ) : null}
+
+            {contact.notes ? <p>{contact.notes}</p> : null}
+
+            <div>
+            <Form action="edit">
+                <button type="submit">Edit</button>
+            </Form>
+
+            <Form
+                action="destroy"
+                method="post"
+                onSubmit={(event) => {
+                const response = confirm(
+                    "Please confirm you want to delete this record."
+                );
+                if (!response) {
+                    event.preventDefault();
+                }
+                }}
+            >
+                <button type="submit">Delete</button>
+            </Form>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 const Favorite: FunctionComponent<{
