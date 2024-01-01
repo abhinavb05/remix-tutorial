8c69e0b2097a6f5e582596fb02d4aa646706dd31
abhinav.bansal <abhinavb0512@gmail.com>
2024-01-01 17:30:35 +0530

    added error handling on 404

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,75 +1,77 @@
 import type { FunctionComponent } from "react";
-import { json } from "@remix-run/node";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
-
 
 import type { ContactRecord } from "../data";
 import { getContact } from "../data";
+import invariant from "tiny-invariant";
 
-export const loader = async ({ params }) => {
-    const contact = await getContact(params.contactId);
-    return json({ contact });
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
 };
 
 export default function Contact() {
-    const { contact } = useLoaderData<typeof loader>();
-    return (
-        <div id="contact">
-        <div>
-            <img
-            alt={`${contact.first} ${contact.last} avatar`}
-            key={contact.avatar}
-            src={contact.avatar}
-            />
-        </div>
+  const { contact } = useLoaderData<typeof loader>();
+  return (
+    <div id="contact">
+      <div>
+        <img
+          alt={`${contact.first} ${contact.last} avatar`}
+          key={contact.avatar}
+          src={contact.avatar}
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter ? (
+          <p>
+            <a href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        ) : null}
+
+        {contact.notes ? <p>{contact.notes}</p> : null}
 
         <div>
-            <h1>
-            {contact.first || contact.last ? (
-                <>
-                {contact.first} {contact.last}
-                </>
-            ) : (
-                <i>No Name</i>
-            )}{" "}
-            <Favorite contact={contact} />
-            </h1>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
 
-            {contact.twitter ? (
-            <p>
-                <a
-                href={`https://twitter.com/${contact.twitter}`}
-                >
-                {contact.twitter}
-                </a>
-            </p>
-            ) : null}
-
-            {contact.notes ? <p>{contact.notes}</p> : null}
-
-            <div>
-            <Form action="edit">
-                <button type="submit">Edit</button>
-            </Form>
-
-            <Form
-                action="destroy"
-                method="post"
-                onSubmit={(event) => {
-                const response = confirm(
-                    "Please confirm you want to delete this record."
-                );
-                if (!response) {
-                    event.preventDefault();
-                }
-                }}
-            >
-                <button type="submit">Delete</button>
-            </Form>
-            </div>
+          <Form
+            action="destroy"
+            method="post"
+            onSubmit={(event) => {
+              const response = confirm(
+                "Please confirm you want to delete this record."
+              );
+              if (!response) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 const Favorite: FunctionComponent<{
@@ -80,11 +82,7 @@ const Favorite: FunctionComponent<{
   return (
     <Form method="post">
       <button
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
         name="favorite"
         value={favorite ? "false" : "true"}
       >
